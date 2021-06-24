da57f23c0f6d729bf61d4a2adddb14ce7d1310c3
gunal333 <p.gunal333@gmail.com>
2021-06-25 02:00:26 +0530

    formValidation commit

--- controllers/unauthorized.js
+++ controllers/unauthorized.js
@@ -18,13 +18,17 @@ exports.postSignUp=(req,res,next)=>{
     User
     .save()  
     .then(result =>{console.log('Account created');
-        res.redirect('/authorized/saved')})
+        res.redirect('/unauthorized/login')})
     .catch(err=>{
+        if(err.code === 11000)
+        {
+            res.render('unauthorized/signup',{duplicate:true,userType:'unauthorized'})
+        }
         console.log(err);
     }) 
 }
 exports.getSignUp=(req,res,next)=>{
-    res.render('unauthorized/signup',{userType:'unauthorized'});
+    res.render('unauthorized/signup',{userType:'unauthorized',duplicate:false});
 }
 exports.getLogin=(req,res,next)=>{
     res.render('unauthorized/login',{userType:'unauthorized'});
