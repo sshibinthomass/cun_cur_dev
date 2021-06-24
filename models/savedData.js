19df5b67632816f732e59ba1a496e0cdd9ce9aef
gunal333 <p.gunal333@gmail.com>
2021-06-24 13:53:02 +0530

    commit newSchema

---
+++ models/savedData.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const savedDataSchema = new schema(
+    {
+         uId :
+         {
+           type : mongoose.ObjectId,
+           required : true  
+         },
+         date: 
+         {
+            type : date,
+            required : true
+         },
+         fromCur:
+         {
+            type : Number,
+            required : true
+         },
+         toCur:{
+            type : Number,
+            required : true
+         },
+         fromVal:{
+            type : Number,
+            required : true
+         },
+         toVal:{
+            type : Number,
+            required : true
+         }
+    }
+);
+module.exports = savedDataSchema
